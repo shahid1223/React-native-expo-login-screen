5a0a0c3095a0f2672aa9e967ae9f77d136de68ab
shahid1223 <ms413959@gmail.com>
2021-12-27 07:47:11 +0530

    remove e.preventddefault from login function

--- Context/LoginState.js
+++ Context/LoginState.js
@@ -3,29 +3,34 @@ import ContexProvider from './ContexProvider'
 
 const LoginState = (props) => {
     // http://localhost:9000/auth/login
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
 
 
-    const login = async (e) => {
-        e.preventDefault();
-        const response = await fetch(" http://localhost:9000/auth/login", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email: email, password: password })
-        });
-        const json = await response.json()
-        console.log(json);
-        if (json.success) {
-            // Save the auth token and redirect
-            localStorage.setItem('token', json.authtoken);
+    const login = async () => {
+        try {
+            // e.preventDefault()
+            const response = await fetch(" http://localhost:9000/auth/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: email, password: password })
+            });
+            const json = await response.json()
+            console.log(json);
+            if (json.success) {
+                // Save the auth token and redirect
+                localStorage.setItem('token', json.authtoken);
 
+            }
+            else {
+                alert("Invalid credentials");
+            }
+        } catch (error) {
+            console.log(error)
         }
-        else {
-            alert("Invalid credentials");
-        }
+
     }
 
     return (
